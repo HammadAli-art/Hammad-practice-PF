eee49fe5c85736d1a4f93e82f56ab4c914b35152
Hammad Babar Ali <hammadbabar0326@gmail.com>
2025-02-11 10:07:18 -0800

    Add files via upload

---
+++ CODE.12 (FOR-LOOP).cpp
@@ -0,0 +1,13 @@
+//12. Find the sum of the first n natural numbers using a for loop:
+#include <iostream>
+using namespace std;
+int main() {
+ int n, sum = 0;
+ cout << "Enter a number: ";
+ cin >> n;
+ for (int i = 1; i <= n; i++) {
+ sum += i;
+ }
+ cout << "The sum of the first " << n << " natural numbers is " << sum << endl;
+ return 0;
+}
