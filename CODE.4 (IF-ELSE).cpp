e55810921c576bd228ed4246061578ab83019d8f
Hammad Babar Ali <hammadbabar0326@gmail.com>
2025-02-11 10:03:25 -0800

    Add files via upload

---
+++ CODE.4 (IF-ELSE).cpp
@@ -0,0 +1,20 @@
+//4. Determine the grade based on marks using multiple if-else:
+#include <iostream>
+using namespace std;
+int main() {
+ int marks;
+ cout << "Enter marks: ";
+ cin >> marks;
+ if (marks >= 90) {
+ cout << "Grade A" << endl;
+ } else if (marks >= 80) {
+ cout << "Grade B" << endl;
+ } else if (marks >= 70) {
+ cout << "Grade C" << endl;
+ } else if (marks >= 60) {
+ cout << "Grade D" << endl;
+ } else {
+ cout << "Fail" << endl;
+ }
+ return 0;
+}
