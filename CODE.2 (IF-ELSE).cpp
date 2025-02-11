efe9dfb17b5747a013fa83c79e2c898090658cda
Hammad Babar Ali <hammadbabar0326@gmail.com>
2025-02-11 10:02:10 -0800

    Add files via upload

---
+++ CODE.2 (IF-ELSE).cpp
@@ -0,0 +1,14 @@
+//2. Check if a number is even or odd using if-else:
+#include <iostream>
+using namespace std;
+int main() {
+ int num;
+ cout << "Enter a number: ";
+ cin >> num;
+ if (num % 2 == 0) {
+ cout << "The number is even." << endl;
+ } else {
+ cout << "The number is odd." << endl;
+ }
+ return 0;
+}
